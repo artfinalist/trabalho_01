d6f7758022888f8058dfffafaa3601801b25a21d
artfinalist <thiagoschultz2@gmail.com>
2025-05-11 20:20:57 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 
 function mostrarMensagem() {
-      document.getElementById("mensagem").innerText = "Obrigado por avaliar o meu primeiro Trabalho na faculdade!";
-    }
+      document.getElementById("mensagem").innerText = "Obrigado por avaliar o meu primeiro Trabalho na faculdade!"; /*a mensagem aparece abaixo favor scrolar o mouse para ver
+    } 
 
